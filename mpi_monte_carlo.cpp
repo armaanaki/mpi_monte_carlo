7114641ce568f0a5c936f88c3fd3bedd20a8e1f5
Armaan Esfahani <armaanaki@gmail.com>
2019-10-28 00:50:44 -0400

    slight changes to correct execution errors
    Signed-off-by: Armaan Esfahani <armaanaki@gmail.com>

--- mpi_monte_carlo.cpp
+++ mpi_monte_carlo.cpp
@@ -7,20 +7,28 @@ double doubleRand(double min, double max);
 
 int main(int argc, char** argv) {
     int my_rank, comm_sz;
-    long long total_tosses, my_hits = 0, total_hits;
 
     // init MPI variables
     MPI_Init(NULL, NULL);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+    long long total_tosses, my_hits = 0, total_hits = 0;
 
     // get the total tosses
     bool error = false;
 
     if (my_rank == 0) {
-        if (argc != 2) error = true;
-        else total_tosses = strtol(argv[1], NULL, 10); 
+        if (argc != 2) {
+            error = true;
+            printf("you must have total tosses as a command arg and nothing else!");
+        } else total_tosses = strtol(argv[1], NULL, 10); 
+
+        if (total_tosses % comm_sz != 0) {
+            error = true;
+            printf("the total tosses must be divisible by total processes!");
+        }
     }
+
 
     // if these was an error, distribute it and terminate all
     MPI_Bcast(&error, 1, MPI_CXX_BOOL, 0, MPI_COMM_WORLD);
@@ -33,7 +41,7 @@ int main(int argc, char** argv) {
     MPI_Bcast(&total_tosses, 1, MPI_LONG, 0, MPI_COMM_WORLD);
 
     // setup the tosses for each process
-    long long my_tosses = comm_sz / total_tosses;
+    long long my_tosses = total_tosses / comm_sz;
 
     for (long long i = 0; i < my_tosses; i++) {
         double x = doubleRand(-1, 1);
@@ -43,7 +51,6 @@ int main(int argc, char** argv) {
         if (distance_squared <= 1) my_hits++;
     }
 
-
     // add all hits to one variable
     MPI_Reduce(&my_hits, &total_hits, 1, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
 
@@ -51,7 +58,9 @@ int main(int argc, char** argv) {
     if (my_rank == 0) {
         double pi_estimate = 4*total_hits/(double) total_tosses;
         printf("pi guess: %.6f\n", pi_estimate);
-    }
+   }
+
+    MPI_Finalize();
 }
 
     
